71889034bcb45bdc22532d405c1aa6534f3e0cfd
Khedr Ibrahim <108041341+KhedrIbrahim@users.noreply.github.com>
2023-01-04 09:11:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+let mySpan = document.querySelector("span");
+let myFirstSpace = mySpan.nextSibling;
+let myComment = myFirstSpace.nextSibling;
+let elzeroWord = myComment.nextSibling;
+console.log(elzeroWord);
